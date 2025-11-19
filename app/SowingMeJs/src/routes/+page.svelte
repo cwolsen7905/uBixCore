4a6db6c2d72f1321e89ea52d4edb50d5829e0f12
Christopher W. Olsen <cwolsen@XenTask.com>
2025-11-19 17:55:08 -0500

    Changing Host Names

--- app/SowingMeJs/src/routes/+page.svelte
+++ app/SowingMeJs/src/routes/+page.svelte
@@ -41,10 +41,6 @@
 	<main class="main">
 		<h1>Welcome to SowingMe Platform</h1>
 		<p>This is your main page content. Add your components and routes here.</p>
+		<pre>DATA: {JSON.stringify($userData?.user, null, 2)}</pre>
 	</main>
 </div>
-
-
-<pre>DATA: {JSON.stringify($userData?.user, null, 2)}</pre>
-
-Hi
