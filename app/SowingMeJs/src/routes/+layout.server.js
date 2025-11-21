06437428ffea3d6fbdd77ba617f671bae1417348
Christopher W. Olsen <cwolsen@XenTask.com>
2025-11-20 22:58:44 -0500

    Passing API Host

--- app/SowingMeJs/src/routes/+layout.server.js
+++ app/SowingMeJs/src/routes/+layout.server.js
@@ -11,12 +11,12 @@ export async function load({ fetch, request}) {
 
   // If environment variable ENV == 'PROD' api host is https://api.sowingme.com if ENV == 'DEV' host is https://sowing-me-api.dev.ubixsys.com if ENV == 'STAGING' host is https://sowing-me-api.staging.ubixsys.com else localhost:8888
   const apiBaseUrl = env.ENV === 'PROD'
-	? 'https://api.sowingme.com/auth'
+	? 'https://api.sowingme.com'
 	: env.ENV === 'DEV'
-	  ? 'https://sowing-me-api.dev.ubixsys.com/auth'
+	  ? 'https://sowing-me-api.dev.ubixsys.com'
 	  : env.ENV === 'STAGING'
-	    ? 'https://sowing-me-api.staging.ubixsys.com/auth'
-	    : 'http://localhost:8888/auth';
+	    ? 'https://sowing-me-api.staging.ubixsys.com'
+	    : 'http://localhost:8888';
 
 const apiEndpoint = `${apiBaseUrl}/auth`;
   
