833e99e669c58eb6fe4ecda767d644e8ae6d115b
Christopher W. Olsen <cwolsen@XenTask.com>
2025-11-21 21:58:12 -0500

    Logging More Data

--- php/Ubix/Middleware/SessionMiddleware.php
+++ php/Ubix/Middleware/SessionMiddleware.php
@@ -65,6 +65,8 @@ final class SessionMiddleware implements Middleware
 
 
 		$isSecure = (getenv('HTTPS') !== false && getenv('HTTPS') !== 'off') || (getenv('HTTP_X_FORWARDED_PROTO') === 'https');
+
+
 		$isSecure = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443;
 		$domain = '.ubixsys.com';
 
@@ -75,6 +77,7 @@ final class SessionMiddleware implements Middleware
 				'secure'   => $isSecure,
 				'httponly' => false,
 				'samesite' => 'Lax', // TEMPORARY: because of the way we're serving the app in development
+				'HTTPS'	 => $_SERVER['HTTPS'] ?? 'not set',
 			]);
 
 		// Invoke session_set_cookie_params() before session_set_save_handler() because the latter will invoke session_get_cookie_params() to get the $domain value
