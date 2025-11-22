af8426afbf142c28895c2a27b1af8e7ba4be1d37
Christopher W. Olsen <cwolsen@XenTask.com>
2025-11-21 22:12:36 -0500

    Updating log path

--- php/Ubix/Middleware/SessionMiddleware.php
+++ php/Ubix/Middleware/SessionMiddleware.php
@@ -68,6 +68,7 @@ final class SessionMiddleware implements Middleware
 
 
 		$isSecure = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443;
+		$isSecure = true;
 		$domain = '.ubixsys.com';
 
 		$this->logger->debug('Session cookie parameters', 			[
